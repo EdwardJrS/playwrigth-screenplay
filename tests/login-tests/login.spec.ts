056a0f49fb356e020396bea9ce2825df0119aa18
unknown <esierrac@choucairtesting.com>
2025-07-14 19:29:49 -0500

    Se termina la configuración del proyecto, se crea primer test del login

---
+++ tests/login-tests/login.spec.ts
@@ -0,0 +1,16 @@
+import {test, expect} from '@playwright/test'
+
+test ('login', async({ page }) => {
+
+  // data to testing
+  const userData = ['standard_user', 'secret_sauce'];
+
+  // open website
+  await page.goto('https://www.saucedemo.com/');
+
+  // login
+  await page.getByPlaceholder('Username').fill(userData[0]);
+  await page.getByPlaceholder('Password').fill(userData[1]);
+  await page.getByText('Login').click();
+
+});
